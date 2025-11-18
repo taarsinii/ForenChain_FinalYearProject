6e508f6cd7adbfe4b88c2803b0aa20fde3f26f91
taarsinii <taarsiniiaarsh@gmail.com>
2025-11-19 04:35:13 +0800

    update to use routes

--- server.js
+++ server.js
@@ -1,34 +1,21 @@
 require("dotenv").config();
 const express = require("express");
-const mysql = require("mysql2");
 const cors = require("cors");
+
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Database connection
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
-});
-
-db.connect((err) => {
-    if (err) {
-        console.error("Database connection failed:", err);
-        return;
-    }
-    console.log("Connected to MySQL database");
-});
-
-// Basic test route
+// Basic route
 app.get("/", (req, res) => {
     res.send("Chain of Custody Backend Running...");
 });
 
-// Start server
+// Load all routes here
+app.use("/api/auth", authRoutes);
+
 app.listen(5000, () => {
-    console.log("Server started on port 5000");
+    console.log("Server running on port 5000");
 });
